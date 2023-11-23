1f2986758675efeba821d6dc90a6fb317163d47d
sahaja <sahajaboyapati@gmail.com>
2023-11-23 15:37:16 +0530

    intial commit

---
+++ RealAgebra/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using ShapeAreaLib;
+namespace RealAgebra
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Select a shape:");
+                Console.WriteLine("1. Rectangle");
+                Console.WriteLine("2. Square");
+                Console.WriteLine("3. Circle");
+                Console.WriteLine("4. Triangle");
+
+                char choice = Console.ReadKey().KeyChar;
+
+                double area = 0;
+
+                switch (choice)
+                {
+                    case '1':
+                        area = GetRectangleArea();
+                        break;
+                    case '2':
+                        area = GetSquareArea();
+                        break;
+                    case '3':
+                        area = GetCircleArea();
+                        break;
+                    case '4':
+                        area = GetTriangleArea();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice.");
+                        return;
+                }
+
+                Console.WriteLine($"Area: {area}");
+            Console.ReadKey();
+        }
+
+            static double GetRectangleArea()
+            {
+                Rectangle rectangle = new Rectangle();
+                Console.Write("Enter length: ");
+                double length = double.Parse(Console.ReadLine());
+                Console.Write("Enter width: ");
+                double width = double.Parse(Console.ReadLine());
+                return rectangle.Area(length, width);
+            }
+
+            static double GetSquareArea()
+            {
+                Square square = new Square();
+                Console.Write("Enter side length: ");
+                double side = double.Parse(Console.ReadLine());
+                return square.Area(side);
+            }
+
+            static double GetCircleArea()
+            {
+                Circle circle = new Circle();
+                Console.Write("Enter radius: ");
+                double radius = double.Parse(Console.ReadLine());
+                return circle.Area(radius);
+            }
+
+            static double GetTriangleArea()
+            {
+                Triangle triangle = new Triangle();
+                Console.Write("Enter base length: ");
+                double baseLength = double.Parse(Console.ReadLine());
+                Console.Write("Enter height: ");
+                double height = double.Parse(Console.ReadLine());
+                return triangle.Area(baseLength, height);
+            }
+        }
+    
+    }
+
+ 
